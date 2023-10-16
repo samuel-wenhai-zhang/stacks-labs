7aa44fc6071a2e8a0ee6a8b05f97b3d9eb275f01
Boltcrasher <Boltcrasher@gmail.com>
2023-10-16 13:07:50 -0500

    3 done

--- Lab3/PostFix.java
+++ Lab3/PostFix.java
@@ -1,7 +1,7 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
+//Name - Samuel Zhang
 
 import java.util.Stack;
 import java.util.Scanner;
@@ -14,24 +14,58 @@ public class PostFix
 
 	public PostFix()
 	{
+		setExpression("");
 	}
 
 	public PostFix(String exp)
 	{
+		setExpression(exp);
 	}
 
 	public void setExpression(String exp)
 	{
+		stack = new Stack<Double>();
+		expression = exp;
 	}
 
 	public double calc(double one, double two, char op)
 	{
-		return 0.0;
+		if (op == '+') {
+			return one + two;
+		}
+		else if (op == '-') {
+			return one - two;
+		}
+		else if (op == '*') {
+			return one * two;
+		}
+		else if (op == '/') {
+			return one / two;
+		}
+		else {
+			return 0;
+		}
 	}
 
 	public void solve()
 	{
+		Scanner in = new Scanner(expression);
+		while (in.hasNext()) {
+			String value = in.next();
+			try {
+				stack.push(Double.parseDouble(value));
+			}
+			catch (NumberFormatException e) {
+				double d2 = stack.pop();
+				double d1 = stack.pop();
+				stack.push(calc(d1, d2, value.charAt(0)));
+			}
+		}
 	}
 
-	//add a toString
+	@Override
+	public String toString() {
+		return expression + " = " + stack.peek();
+	}
+
 }
